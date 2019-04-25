7e72fc00de2f83d03f6cd462ae12a90104e25490
SvenOu <948077475@qq.com>
2019-04-25 15:22:14 +0800

    fix bug

--- application/src/main/java/com/sven/codegenerator/configs/CodeGeneratorConfig.java
+++ application/src/main/java/com/sven/codegenerator/configs/CodeGeneratorConfig.java
@@ -9,7 +9,8 @@ public class CodeGeneratorConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         String loginPageUrl = "/webs/security/login.html";
-        registry.addViewController("/").setViewName(loginPageUrl);
+        registry.addRedirectViewController("/",loginPageUrl);
+//        registry.addViewController("/").setViewName(loginPageUrl);
  //                registry.addViewController("/302").setViewName("forward:/login.html");
 //                registry.addViewController("/304").setViewName("forward:/login.html");
 //                registry.addViewController("/403").setViewName("forward:/login.html");
